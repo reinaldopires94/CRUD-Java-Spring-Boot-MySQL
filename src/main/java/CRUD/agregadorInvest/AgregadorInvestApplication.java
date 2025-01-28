3890f42c7bd292b45d3b0d15009d172c8b48ba19
reinaldopires94 <reinaldopires94@hotmail.com>
2025-01-28 14:15:47 +0000

    Inicialização do CRUD

---
+++ src/main/java/CRUD/agregadorInvest/AgregadorInvestApplication.java
@@ -0,0 +1,13 @@
+package CRUD.agregadorInvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadorInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadorInvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CRUD/agregadorInvest/AgregadorInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package CRUD.agregadorInvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
